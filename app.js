65cfb75574ac148b8a8af6791bf4576fc3be8fa0
SleptsovKonstantin <sleptsov.exceedteam@gmail.com>
2022-04-12 11:28:15 +0300

    add mpdels db and migrations

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -7,10 +8,15 @@ app.use(cors());
 app.use(express.json());
 
 require("./src/routes/index")(app);
+require("./src/routes/record")(app);
 
-db.sequelize.sync();  //{force: true} для удаления
+db.sequelize.sync();
+// db.sequelizeNew.sync();
+// db.sequelize.sync({ force: true }).then(() => {  //для удаления
+//   console.log("Drop and re-sync db.");
+// });
 
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}.`);
 });
