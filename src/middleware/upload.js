7c6e853220b050f2f5819fcb7a4d8b728272cc99
Константин Слепцов <konstantinslepcov@MacBook-Air-Konstantin.local>
2022-04-22 21:07:22 +0300

    add regular and trim

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,9 +1,10 @@
 const patternPas = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/; //латиница и цифры
+const lenghtPass = /^[0-9a-zA-Z]{6,}$/;
 
 const passwordCheck = (req, res, next) => {
   const body = req.body;
   const { login, password } = body;
-  if (login.length >= 6 && patternPas.test(password) && password.length >= 6) {
+  if (lenghtPass.test(login.trim()) && patternPas.test(password) && lenghtPass.test(password.trim())) {
     next();
   } else {
     res.send(`Error, check correct user`);
