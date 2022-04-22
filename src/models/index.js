29b3b9be15a70b96c5c53307eac98a450b5ea5fa
Константин Слепцов <konstantinslepcov@MacBook-Air-Konstantin.local>
2022-04-22 16:38:41 +0300

    fix bugs 2

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
-const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
+const dbConfig = require("../config/db.config");
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
